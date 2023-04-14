b1d2f56eee422339c4e23e8a9c300bbf4b4e552e
pulkitk07 <117850942+pulkitk07@users.noreply.github.com>
2023-04-13 23:06:41 -0700

    Add files via upload

---
+++ Problem.py
@@ -0,0 +1,53 @@
+import os
+import sqlite3
+import time
+
+# Path to the directory containing the weather data files
+data_dir = "/Volumes/Pulkit/code-challenge-template-main/wx_data"
+
+# Connect to the database
+conn = sqlite3.connect("weather.db")
+
+# Create a table for the weather data
+create_table_sql = """
+CREATE TABLE IF NOT EXISTS weather_data (
+    
+    station_id INTEGER NOT NULL,
+    date INTEGER NOT NULL,
+    max_temp REAL NOT NULL,
+    min_temp REAL NOT NULL,
+    precipitation REAL NOT NULL,
+    PRIMARY KEY (station_id, date, max_temp, min_temp, precipitation)
+)
+"""
+conn.execute(create_table_sql)
+
+start_time = time.time()
+
+
+# Iterate through the data files and insert data into the database
+for filename in os.listdir(data_dir):
+    if filename.startswith("USC"):
+        station_id = filename[3:6]
+        with open(os.path.join(data_dir, filename), "r") as f:
+            for line in f:
+                date, max_temp, min_temp, precipitation = line.strip().split("\t")
+                # Skip lines with missing data
+                if max_temp == "-9999" or min_temp == "-9999" or precipitation == "-9999":
+                    continue
+                insert_sql = """
+                INSERT OR IGNORE INTO weather_data
+                (station_id, date, max_temp, min_temp, precipitation)
+                VALUES (?, ?, ?, ?, ?)
+                """
+                conn.execute(insert_sql, (int(station_id), int(date), int(max_temp)/10, int(min_temp)/10, int(precipitation)/10))
+conn.commit()
+
+# Print the number of records ingested
+select_count_sql = "SELECT COUNT(*) FROM weather_data"
+print(f"Number of records ingested: {conn.execute(select_count_sql).fetchone()[0]}")
+
+# Close the connection
+conn.close()
+end_time = time.time()
+print(f'Records were ingested in {end_time - start_time:.2f} seconds')
---
+++ ReadMe.txt
@@ -0,0 +1,12 @@
+This project has been created by Pulkit Khursija
+This project contains the following files:-
+"Paragon Python Test.pdf" This file contains the problem question for which the solution has been made
+"problem1.txt" This file contains solution to problem 1. It has the SQL DDL statements.
+"problem.py" This file contains python code to solve problem 2. It Ingests data into the DB without replication if run multiple times.
+"problem3.txt" This file contains solution to problem 3. It contains SQL commands to solve problem 3.
+"app.py" This file contains python code to solve problem 4. It creates REST API for the problem using GET methods.
+"test_app.py" This file contains one test case for /api endpoint.
+"test_unit.py" This file contains 3 test cases for /api/weather and /api/weather/stats gateways.
+"swagger.yaml" This file contains doccumentaion of my API
+"ExtraCredit.txt: This file contains solution to the Extra Credit problem. I explain the few AWS tools that I could use for this project.
+"requirements.txt" This file can be accessed to create an environment that is used while developing it.
